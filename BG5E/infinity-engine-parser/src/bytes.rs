4fbc33fe4b98b3e02e8bbd67af326a8cea6bf8a2
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-09-12 15:03:26 -0400

    Update bytes.rs
    Removed readU32 macro.
    Added readBytes macro, for reading an arbitrary number of bytes from a std::io::Cursor.

--- BG5E/infinity-engine-parser/src/bytes.rs
+++ BG5E/infinity-engine-parser/src/bytes.rs
@@ -2,13 +2,39 @@
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
 /**
+Read an arbitrary number of bytes from a `std::io::Cursor` instance.
+
+---
+
+Parameter | Description
+---|---
+$cursor | The cursor being read.
+$length | The number of bytes to read.
+*/
+#[macro_export]
+macro_rules! readBytes
+{
+	($cursor:expr, $length:expr) => {
+		{
+			let mut nameBytes = vec![];
+			for _ in 0..$length
+			{
+				let bite = $cursor.read_u8()?;
+				nameBytes.push(bite);
+			}
+			nameBytes
+		}
+	}
+}
+
+/**
 Convert an array of bytes into a String.
 
 ---
 
 Parameter | Description
---- | ---
-bytes | The array of bytes to be converted. Expects a slice.
+---|---
+$bytes | The array of bytes to be converted. Expects a slice.
 */
 #[macro_export]
 macro_rules! readString
@@ -17,34 +43,5 @@ macro_rules! readString
 		String::from_utf8($bytes.into())
 			.map_err(|nonUtf8| String::from_utf8_lossy(nonUtf8.as_bytes()).into_owned())
 			.unwrap()
-	}
-}
-
-/**
-Convert four bytes into a u32 value.
-
-Returns a std::io::Error to the containing scope if the conversion from slice
-to fixed-size array fails.
-
----
-
-Parameter | Description
---- | ---
-bytes | The array of bytes to be converted. Expects a slice.
-*/
-#[macro_export]
-macro_rules! readU32
-{
-	($bytes:expr) => {
-		{
-			let val;
-			match $bytes.try_into()
-			{
-				//TODO: Handle variable Endian-ness
-				Ok(buf) => val = u32::from_le_bytes(buf),
-				Err(e) => return Err(std::io::Error::new(std::io::ErrorKind::Other, e)),
-			}
-			val
-		}
 	}
 }
