26011410e907c0d19fe7937c03419dea03566da6
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-09-12 19:56:42 -0400

    Update util.rs
    Added InfinityEngineType trait.
    Added generic ReadFromFile<T> where T: InfinityEngineType, since the fromFile method would have been essentially identical across all of these types anyway.

--- BG5E/infinity-engine-parser/src/types/util.rs
+++ BG5E/infinity-engine-parser/src/types/util.rs
@@ -1,10 +1,48 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+use std::fs;
+use std::io;
 use std::io::Cursor;
 use std::path::Path;
 use byteorder::ReadBytesExt;
 use crate::{readBytes, readString};
+
+/**
+A data type which can be found in and read from Infinity Engine game files.
+*/
+pub trait InfinityEngineType
+{
+	type Output;
+	
+	/**
+	Create a new instance of type `T` based on the data contained in `cursor`.
+	
+	---
+	
+	### Parameters
+	- **cursor** - The cursor from which to read data.
+	*/
+	fn fromCursor<T>(cursor: &mut Cursor<Vec<u8>>) -> io::Result<Self::Output>
+		where T: InfinityEngineType;
+}
+
+/**
+Create a new instance of type `T` based on the data contained in `file`.
+
+---
+
+### Parameters
+- **file** - The fully qualified path to the file being read.
+*/
+pub fn ReadFromFile<T>(file: &Path) -> io::Result<T::Output>
+	where T: InfinityEngineType
+{
+	let buffer = fs::read(file)?;
+	let mut cursor = Cursor::new(buffer);
+	
+	return T::fromCursor::<T>(&mut cursor);
+}
 
 /**
 Simple data structure containing only the Signature and Version of a file. Used
