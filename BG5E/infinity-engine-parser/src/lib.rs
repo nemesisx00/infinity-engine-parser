0634b60cb4a310a3f7672affbe6ae69cef2653e9
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-09-12 12:32:36 -0400

    Update lib.rs

--- BG5E/infinity-engine-parser/src/lib.rs
+++ BG5E/infinity-engine-parser/src/lib.rs
@@ -1,9 +1,10 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+mod bits;
+mod bytes;
 mod key;
 mod game;
-mod util;
 
 use ::safer_ffi::prelude::*;
 
