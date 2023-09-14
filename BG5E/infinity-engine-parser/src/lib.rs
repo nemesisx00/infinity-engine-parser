eb86b39ff653ec46b21a2279ceae93afb117ab78
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-09-14 14:29:44 -0400

    Update lib.rs
    Set up a first pass at a real FFI interface.

--- BG5E/infinity-engine-parser/src/lib.rs
+++ BG5E/infinity-engine-parser/src/lib.rs
@@ -7,8 +7,39 @@ mod platform;
 mod resource;
 mod types;
 
+use ::image::ImageOutputFormat;
 use ::safer_ffi::prelude::*;
+use platform::Games;
+use resource::ResourceManager;
+use types::Bmp;
 
+#[ffi_export]
+fn loadBmp(gameValue: u32, resourceName: char_p::Ref<'_>) -> repr_c::Vec<u8>
+{
+	let name = resourceName.to_str();
+	let mut data = vec![];
+	if let Some(game) = Games::from_repr(gameValue)
+	{
+		let mut resourceManager = ResourceManager::default();
+		if let Some(bmp) = resourceManager.loadFileResource::<Bmp>(game, name.to_owned())
+		{
+			if let Ok(image) = bmp.toImageBytes(Some(ImageOutputFormat::Png))
+			{
+				data = image;
+			}
+		}
+	}
+	
+	return data.into();
+}
+
+#[ffi_export]
+fn freeBytes(data: repr_c::Vec<u8>)
+{
+	drop(data);
+}
+
+/*
 #[ffi_export]
 fn realTest() -> char_p::Box
 {
@@ -19,8 +50,8 @@ fn realTest() -> char_p::Box
 }
 
 #[ffi_export]
-fn getBytes() -> repr_c::Vec<u8>
+fn freeRealTest(msg: char_p::Box)
 {
-	let data = vec![4,5,3,6,2,7,1,8];
-	return data.into();
+	drop(msg);
 }
+*/
