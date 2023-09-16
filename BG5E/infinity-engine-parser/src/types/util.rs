69475a9758ecf0ca880298e4715bea5bdf5d2399
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-09-16 16:36:51 -0400

    Update util.rs
    Added Dimensions struct.

--- BG5E/infinity-engine-parser/src/types/util.rs
+++ BG5E/infinity-engine-parser/src/types/util.rs
@@ -6,6 +6,7 @@ use std::io::Cursor;
 use std::path::Path;
 use ::anyhow::{Result, Context};
 use ::byteorder::ReadBytesExt;
+use ::safer_ffi::derive_ReprC;
 use crate::{readBytes, readString};
 
 /**
@@ -43,6 +44,30 @@ pub fn ReadFromFile<T>(file: &Path) -> Result<T::Output>
 	let mut cursor = Cursor::new(buffer);
 	
 	return T::fromCursor::<T>(&mut cursor);
+}
+
+/**
+Data structure for passing a resource's height and width across the FFI border.
+*/
+#[derive_ReprC]
+#[repr(C)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+pub struct Dimensions
+{
+	pub height: i32,
+	pub width: i32,
+}
+
+impl Dimensions
+{
+	pub fn new(height: i32, width: i32) -> Self
+	{
+		return Self
+		{
+			height,
+			width,
+		};
+	}
 }
 
 /**
