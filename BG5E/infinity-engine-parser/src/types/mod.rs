21334dc229cfe689f17a08e50ddc12e9b383bce2
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-09-22 14:03:20 -0400

    Update mod.rs
    Added are, tis, and wed modules with their public use statements and resource type constants.

--- BG5E/infinity-engine-parser/src/types/mod.rs
+++ BG5E/infinity-engine-parser/src/types/mod.rs
@@ -1,17 +1,23 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+mod are;
 mod bif;
 mod bmp;
 mod key;
+mod tis;
 mod util;
+mod wed;
 
+pub use are::Are;
 pub use bif::Bif;
 pub use bmp::Bmp;
 pub use key::Key;
+pub use tis::Tis;
 pub use util::{Dimensions, Identity, InfinityEngineType, ReadFromFile};
 
 pub use bif::{
+	ResourceType_ARE,
 	ResourceType_BAM,
 	ResourceType_BAMC,
 	ResourceType_BMP,
@@ -20,4 +26,5 @@ pub use bif::{
 	ResourceType_TIS,
 	ResourceType_WAV,
 	ResourceType_WAVC,
+	ResourceType_WED,
 };
