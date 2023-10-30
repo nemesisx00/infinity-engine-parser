272ff831b41aadbe24bc234083026b54e45604ce
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-10-30 16:46:31 -0400

    Update bytes.rs
    Added function readToReal to omit any garbage data when reading bytes which are meant to be parsed as a string.
    
    Renamed readString to parseString.
    
    Added readBytesExact to make reading via cursor.read_exact less verbose.
    
    Implemented a new readString to be a one-step process, utilizing readBytesExact, readToReal, and parseString.
    
    Updated readResRef and readName to rely on readString.

--- src/bytes.rs
+++ src/bytes.rs
@@ -1,6 +1,40 @@
 #![allow(non_snake_case, non_upper_case_globals)]
 #![cfg_attr(debug_assertions, allow(dead_code))]
 
+use std::io::Cursor;
+use anyhow::Error;
+use ::anyhow::Result;
+use crate::types::TypeSize_RESREF;
+
+pub const StringNameLength: usize = 32;
+
+/**
+Convert an array of bytes into a String.
+
+---
+
+Parameter | Description
+---|---
+$bytes | The array of bytes to be converted. **Expects a slice.**
+*/
+#[macro_export]
+macro_rules! parseString
+{
+	($bytes:expr) => {
+		{
+			let parsed = String::from_utf8($bytes.into())
+				.map_err(|nonUtf8| String::from_utf8_lossy(nonUtf8.as_bytes()).to_string());
+			
+			match parsed
+			{
+				Ok(success) => success,
+				Err(notSuccess) => notSuccess,
+			}
+				.replace("\0", "")
+		}
+	}
+}
+
 /**
 Read an arbitrary number of bytes from a `std::io::Cursor` instance.
 
@@ -9,7 +43,7 @@ Read an arbitrary number of bytes from a `std::io::Cursor` instance.
 Parameter | Description
 ---|---
 $cursor | The cursor being read.
-$length | The number of bytes to read.
+$length | The amount of bytes to read.
 */
 #[macro_export]
 macro_rules! readBytes
@@ -28,21 +62,143 @@ macro_rules! readBytes
 }
 
 /**
-Convert an array of bytes into a String.
+Read an arbitrary number of bytes from a `std::io::Cursor` instance.
 
 ---
 
 Parameter | Description
 ---|---
-$bytes | The array of bytes to be converted. Expects a slice.
+$cursor | The cursor being read.
+$length | The amount of bytes to read. **Must be constant or literal!**
+*/
+#[macro_export]
+macro_rules! readBytesExact
+{
+	($cursor:expr, $length:expr) => {
+		{
+			use std::io::Read;
+			
+			let mut bytes: [u8; $length] = [0; $length];
+			$cursor.read_exact(&mut bytes)?;
+			
+			bytes
+		}
+	}
+}
+
+/**
+Read an arbitrary number of bytes from a `std::io::Cursor` and convert them into
+a `String`.
+
+The list of bytes is first filtered to only include those bytes which occur
+before the first NUL value. Then the resultant String also has any NUL characters
+trimmed before returning.
+
+---
+
+Parameter | Description
+---|---
+$cursor | The `Cursor` instance from which to read the bytes.
+$length | The amount of bytes to read. **Must be constant or literal!**
 */
 #[macro_export]
 macro_rules! readString
 {
-	($bytes:expr) => {
-		String::from_utf8($bytes.into())
-			.map_err(|nonUtf8| String::from_utf8_lossy(nonUtf8.as_bytes()).into_owned())
-			.unwrap()
-			.replace("\0", "")
+	($cursor:expr, $length:expr) => {
+		{
+			use crate::{parseString, readBytesExact};
+			use crate::bytes::readToReal;
+			
+			let bytes = readBytesExact!($cursor, $length);
+			let mut realBytes: [u8; $length] = [0; $length];
+			readToReal(&bytes, &mut realBytes)?;
+			
+			parseString!(bytes)
+		}
 	}
+}
+
+/**
+Read a string, the size of a RESREF value (8 bytes), from a `std::io::Cursor`
+instance.
+
+Any NUL characters are removed from the string before returning.
+
+---
+
+Parameter | Description
+---|---
+cursor | The cursor from which to read the string.
+
+---
+
+#### Note
+
+The cursor's position is not updated before reading.
+*/
+pub fn readResRef(cursor: &mut Cursor<Vec<u8>>) -> Result<String>
+{
+	let resref = readString!(cursor, TypeSize_RESREF);
+	return Ok(resref);
+}
+
+/**
+Read a string, the size of a typical name value (32 bytes), from a
+`std::io::Cursor` instance.
+
+Any NUL characters are removed from the string before returning.
+
+---
+
+Parameter | Description
+---|---
+cursor | The cursor from which to read the string.
+
+---
+
+#### Note
+
+The cursor's position is not updated before reading.
+*/
+pub fn readName(cursor: &mut Cursor<Vec<u8>>) -> Result<String>
+{
+	let name = readString!(cursor, StringNameLength);
+	return Ok(name);
+}
+
+/**
+Fill the `real` byte array with values from the `read` byte array in order until
+a NUL is found.
+
+#### Note
+
+Strings stored in Infinity Engine resources are NUL terminated and are not
+guaranteed to fill up the entire possible length available in the binary data.
+Thus only values up to the first NUL should be considered valid data with any
+remaining values being considered garbage to be discarded.
+*/
+pub fn readToReal(read: &[u8], real: &mut [u8]) -> Result<()>
+{
+	if read.len() != real.len()
+	{
+		return Err(
+			Error::new(
+				std::io::Error::new(
+					std::io::ErrorKind::InvalidData,
+					format!("Byte arrays are not the same length! {} != {}", read.len(), real.len())
+				)
+			)
+		);
+	}
+	
+	for (i, byte) in read.iter().enumerate()
+	{
+		match *byte > 0
+		{
+			true => real[i] = *byte,
+			false => break,
+		}
+	}
+	
+	return Ok(());
 }
