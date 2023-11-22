0761111f355e294de9e3627bc4fb572b13b436fc
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-22 15:57:39 -0500

    Create linux.rs
    Added a stub linux::FindInstallationPath function.

---
+++ src/platform/linux.rs
@@ -0,0 +1,10 @@
+#![allow(non_snake_case, non_upper_case_globals)]
+#![cfg_attr(debug_assertions, allow(dead_code))]
+
+use super::global::{Games, GogGameId, SteamAppId};
+
+pub fn FindInstallationPath(game: Games) -> Option<String>
+{
+	//TODO: Actually implement this
+	return None;
+}
