b1544b10c2515fdce26c7259926af38116907ac0
Peter Lunneberg <1031458+nemesisx00@users.noreply.github.com>
2023-11-22 15:13:39 -0500

    Update boundingbox.rs
    Updated BoundingBox::fromCursor to read each value directly from the cursor, rather than adding the step of parsing a u64 in another method.
    
    Updated BoundingBox::from to correctly parse the u64 value.

--- src/types/util/boundingbox.rs
+++ src/types/util/boundingbox.rs
@@ -20,10 +20,10 @@ impl From<u64> for BoundingBox
 {
     fn from(value: u64) -> Self
 	{
-		let left = ReadValue(value.into(), 16, 48) as u16;
-		let top = ReadValue(value.into(), 16, 32) as u16;
-		let right = ReadValue(value.into(), 16, 16) as u16;
-		let bottom = ReadValue(value.into(), 16, 0) as u16;
+		let left = ReadValue(value.into(), 16, 0) as u16;
+		let top = ReadValue(value.into(), 16, 16) as u16;
+		let right = ReadValue(value.into(), 16, 32) as u16;
+		let bottom = ReadValue(value.into(), 16, 48) as u16;
 		
 		return Self
 		{
@@ -40,8 +40,17 @@ impl Readable for BoundingBox
 	fn fromCursor(cursor: &mut Cursor<Vec<u8>>) -> Result<Self>
 		where Self: Sized
 	{
-		let value = cursor.read_u64::<LittleEndian>()?;
-		return Ok(Self::from(value));
+		let left = cursor.read_u16::<LittleEndian>()?;
+		let top = cursor.read_u16::<LittleEndian>()?;
+		let right = cursor.read_u16::<LittleEndian>()?;
+		let bottom = cursor.read_u16::<LittleEndian>()?;
+		
+		return Ok(Self
+		{
+			bottom,
+			left,
+			right,
+			top,
+		});
 	}
 }
-
